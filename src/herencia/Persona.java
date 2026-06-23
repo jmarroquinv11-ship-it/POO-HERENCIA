b2e3e815efde9ecf462102fb12752e7cf76b8310
Juanpii_02 <juanpablvasquez1234@gmail.com>
2026-06-22 18:24:27 -0600

    Inicio proyecto herencia

---
+++ src/herencia/Persona.java
@@ -0,0 +1,17 @@
+package herencia;
+
+public class Persona {
+
+    protected String nombre;
+    protected int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void mostrar() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+    }
+}
